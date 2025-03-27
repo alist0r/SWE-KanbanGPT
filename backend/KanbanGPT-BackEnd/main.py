df06f3f9971c8d0b578dce58ff9a15e4c4f7c388
bsparks96 <109232300+bsparks96@users.noreply.github.com>
2025-03-26 21:33:56 -0400

    Add files via upload

--- backend/KanbanGPT-BackEnd/main.py
+++ backend/KanbanGPT-BackEnd/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
-#from routes import orderRoute, ingredientRoute, batchRoute, locationRoute, recipeRoute, grnRoute, productRoute
-#from routes import userRoute, dashboardRoute
+from routes import users
 from utils.database import engine
 from models import models
 import logging
@@ -12,24 +11,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 app = FastAPI(title="KanbanGPT")
-#app.include_router(receipyRoute.router)
-#app.include_router(ingredientRoute.router)
-#app.include_router(userRoute.router)
-#app.include_router(orderRoute.router)
+app.include_router(users.router, prefix="/api", tags=["Users"])
 
-#app.include_router(batchRoute.router)
-
-#app.include_router(locationRoute.router)
-
-#app.include_router(recipeRoute.router)
-
-#app.include_router(grnRoute.router)
-
-#app.include_router(productRoute.router)
-
-#app.include_router(dashboardRoute.router)
-
-#models.Base.metadata.create_all(bind=engine)
 
 origins = ["*"]
 
